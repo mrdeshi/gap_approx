f112db86cd1c5eab69bce10efe6109f971b8d55c
mrdeshi <desimonifil@gmail.com>
2026-03-01 20:01:28 +0100

    solutions tests and printing

--- main.py
+++ main.py
@@ -12,8 +12,9 @@ import numpy as np
 # n = M.shape[1]
 # m = M.shape[0]
 # instance = InstanceRestrictedAssignment(n, m, generate=False, M = M)
-instance = Covering(2,15, [3, 6, 12, 24, 17, 33, 66, 132, 264, 528, 160, 640, 576, 320, 288])
+instance = Covering(3,15, [3, 6, 12, 24, 17, 33, 66, 132, 264, 528, 160, 640, 576, 320, 288])
 sol, C_max = instance.opt_IP()
-print(f"Optimal C_max: {C_max}")
+print(f"Optimal C_max: {C_max}, solutions {sol}")
 C_max_LP = instance.opt_LP(verbose=True, C_max=C_max)
-print(f"Optimal C_max LP relaxation: {C_max_LP[0]}")
+print(f"Optimal C_max LP relaxation: {C_max_LP[0]} , solutions {C_max_LP[0]}")
+
