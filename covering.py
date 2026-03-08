e33f82733bd2ba433e30ba1ba94da0a902cb3d56
mrdeshi <desimonifil@gmail.com>
2026-03-08 16:31:20 +0100

    new algorithm + half integrality lp configuration:
    check if a feasible solution can be written as half-integral, with pre calculted configurations or column generation modes

--- covering.py
+++ covering.py
@@ -1,5 +1,6 @@
 from pyscipopt import Model
 from itertools import combinations
+from itertools import product
 import numpy as np
 """
 We consider the case separately when there are just 2 machines. In that case, the problem is equivalent
@@ -119,28 +120,28 @@ class Covering:
 
         # Decision variables
         x = {}
-        w = {}
+        #w = {}
         for c in configs:
-            x[c] = model.addVar(vtype="C", name=f"x({c})", lb=0.0)
-            w[c] = model.addVar(vtype="I", name=f"w({c})", lb=0.0)
+            x[c] = model.addVar(vtype="I", name=f"2x({c})", lb=0.0)
+            #w[c] = model.addVar(vtype="I", name=f"w({c})", lb=0.0)
         
 
         # The sum of the variables is at most 2.
-        model.addCons(sum(x[c] for c in configs) <= self.n_machines)
+        model.addCons(sum(x[c]/2 for c in configs) <= self.n_machines)
     
         # half integrality
         for c in configs:
-            model.addCons(2*x[c]==w[c])
-            model.addCons(w[c]<=2)
+            #model.addCons(2*x[c]==w[c])
+            model.addCons(x[c]<=2)
 
         # Each job gets allocated at least once
         for j in range(self.n_jobs):
-            model.addCons(sum(x[c] for c in configs if j in c) >= 1)
+            model.addCons(sum(x[c]/2 for c in configs if j in c) >= 1)
 
         model.optimize()
 
         if model.getStatus() == 'optimal':
-            x_val = dict(zip(x.keys(), [model.getVal(x[e]) for e in x.keys()]))
+            x_val = dict(zip(x.keys(), [model.getVal(x[e]/2) for e in x.keys()]))
             return True, x_val
         return False, {}
     
@@ -148,7 +149,106 @@ class Covering:
     def solve(self, T, job_pair=[-1, -1]):
         print("to implement")
 
+    def find_half_integral_persistence(self,dictionary, column_generation=True):
+        """
+        :param dictionary: the feasible solution
+        :return: True if there is any arbitrary reformulation of y that is half-integral given a persistent, feasble configuration, otherwise False
+        """
+        clean = {} #dictionary with only configurations that has positive non-zero value 
+        if(not dictionary):
+            print("not feasible")
+            return
+        print("start with \n")
+        for k in dictionary.keys():
+            if (dictionary.get(k)>0.0000000000000001):
+                clean[k] = dictionary.get(k)
+                print("subset=",k,"ibrid machine=",dictionary.get(k))
 
+        complete = {} #dictionary for each job the % of completeness in {0,0.5,1}
+    
+
+        for set in clean.keys():
+            clean[set] = 0
+        
+        for job in range(self.n_jobs):
+            complete[job] = 0
+        
+        n_subsets= len(clean.keys())
+
+        if column_generation:
+            return self.config_find(0,clean,complete,0,0)
+
+        for solution in product([0,0.5,1],repeat=n_subsets):
+            i = 0
+            for  key in clean.keys():
+                clean[key] = solution[i]
+                i+=1
+            
+            for s in clean.keys():
+                value = clean[s]
+                for j in s:
+                    complete[j] += value
+
+    
+            check = True
+            total_power=0
+            for s in clean.keys():
+                total_power += clean[s]
+            
+            if (total_power> self.n_machines):
+                check = False
+            for job in range(self.n_jobs):
+                if (complete[job]<1):
+                    check = False
+            
+            if (check):
+                return clean
+            
+            for job in range(self.n_jobs):
+                complete[job] = 0
+            
+        return False
+
+
+    #with column generation
+    def config_find(self, v, clean, complete, power, step):
+
+        if v>=1.5:
+            return False
+
+        check = True
+        total_power=0
+        for s in clean.keys():
+            total_power += clean[s]
+        
+        if (total_power> self.n_machines):
+            return False
+        
+        for job in range(self.n_jobs):
+            if (complete[job]<1):
+                check = False
+        
+        if (check):
+            return clean
+    
+        elif(step==len(list(clean.keys()))):
+            return False
+        
+
+        p = power
+        clean_cop = clean.copy()
+        complete_cop = complete.copy()
+        subset = list(clean_cop.keys())[step] #
+        clean_cop[subset] = v
+        p+=v
+        for j in subset:
+            complete_cop[j] += v
+
+        
+
+        return self.config_find(v+0.5,clean,complete,power, step) or self.config_find(0,clean_cop,complete_cop,p, step+1)
+
+        
 
     def opt_IP(self, verbose=False):
         model = Model('Restricted assignment with 2 processing times')
